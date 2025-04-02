86b41b78c2ce981dca7c82618c3ec76c597265eb
Simone Ausilio (PhD Dept ElecElecComp Eng FT) <sxa1938@student.bham.ac.uk>
2025-04-02 10:49:27 +0100

    Updated requirements and test_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,8 @@
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_mean
+import pytest
+from inflammation.models import daily_mean, daily_max, daily_min
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,3 +30,28 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_max():
+    """Tests that max function works for an array of positive integers."""
+
+    test_input = np.array([[1, 8, 3],
+                           [7, 5, 6],
+                           [4, 2, 9]])
+    test_result = np.array([7, 8, 9])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min():
+    """Tests that min function works for an array of positive integers."""
+
+    test_input = np.array([[1, 8, 3],
+                           [7, 5, 6],
+                           [4, 2, 9]])
+    test_result = np.array([1, 2, 3])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_string():
+    """Tests for TypeError when passing strings."""
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
