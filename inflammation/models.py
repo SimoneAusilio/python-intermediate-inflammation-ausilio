395ae0fce86366e5674e3d764f28d68f8e36c814
Simone Ausilio (PhD Dept ElecElecComp Eng FT) <sxa1938@student.bham.ac.uk>
2025-04-29 11:05:59 +0100

    Fixes after first review

--- inflammation/models.py
+++ inflammation/models.py
@@ -33,12 +33,12 @@ def daily_min(data):
     return np.min(data, axis=0)
 
 
-def s_dev(data):
+def daily_std(data):
     """Computes and returns standard deviation for data."""
-    mmm = np.mean(data, axis=0)
+    m = np.mean(data, axis=0)  # mean of each column
     devs = []
     for entry in data:
-        devs.append((entry - mmm) * (entry - mmm))
+        devs.append((entry - m) * (entry - m))
 
     s_dev2 = sum(devs) / len(data)
     return {'standard deviation': s_dev2}
